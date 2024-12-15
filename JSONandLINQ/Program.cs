3c66abf9e50ed68c235460f13aa4fbfa3c58dc67
Kemran <morpeh2005ka@gmail.com>
2024-12-15 13:59:21 +0300

    Сделал LINQ и JSON

---
+++ JSONandLINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace JSONandLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass[] array = new SomeClass[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Random rand = new Random();
+                array[i] = new SomeClass(rand.Next(), rand.Next().ToString());
+            }
+            var LINQExample = from clss in array
+                              where clss != null
+                              where clss.number > 3
+                              select clss;
+            foreach (var clss in LINQExample)
+            {
+                Console.WriteLine("Сериализованный JSON:");
+                string json = JsonConvert.SerializeObject(clss);
+                Console.WriteLine(json);
+                SomeClass deserializedPerson = JsonConvert.DeserializeObject<SomeClass>(json);
+                Console.WriteLine("Десериализованный объект:");
+                Console.WriteLine($"Name: {deserializedPerson.number}, Age: {deserializedPerson.str}\n");
+            }
+        }
+    }
+}
---
+++ JSONandLINQ/SomeClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONandLINQ
+{
+    /// <summary>
+    /// Some class
+    /// </summary>
+    public class SomeClass
+    {
+        /// <summary>
+        /// Some number
+        /// </summary>
+        public int number = 0;
+        /// <summary>
+        /// Some string
+        /// </summary>
+        public string str = "";
+        /// <summary>
+        /// Some class
+        /// </summary>
+        /// <param name="a"></param>
+        public SomeClass(int a, string b)
+        {
+            number = a;
+            str = b;
+        }
+        /// <summary>
+        /// Get int param
+        /// </summary>
+        /// <returns></returns>
+        public int ToInt() => number + str.Length;
+        /// <summary>
+        /// Get str param
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => number.ToString() + str;
+    }
+}
